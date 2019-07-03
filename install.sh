a7f7ac549aaf35d53450e10e9bf571cc47d20008
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-07-03 07:37:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/Bash
+rm -rf node_modules
 echo "[!] Installing package"
-sleep(1)
+sleep 1
 apt-get install nodejs -y
 npm install shelljs colors chalk cowsay readline request ora beeper log-symbols fs
 node spoof.js
