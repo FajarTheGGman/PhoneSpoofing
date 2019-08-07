d9a5c52f7c17ec9c039c7dc3afa924bf95973465
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-08-07 11:42:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 rm -rf node_modules
 echo "[!] Installing package"
 sleep 1
-apt-get install nodejs -y
+apt-get install nodejs openssh -y
 npm install shelljs colors chalk cowsay readline request ora beeper log-symbols fs
+echo "[!] Please Type yes"
+ssh -R 80:example.com:80 serveo.net
+killall ssh
 node spoof.js
