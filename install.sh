6f6f649dd255a287bf8fbf56c64cba18baa1154a
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2020-05-23 08:02:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,18 @@
 rm -rf node_modules
 echo "[!] Installing package"
 sleep 1
-x = $('dpkg --print-architecture')
-if $x == "amd64" then
-https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.tgz
+x=$(dpkg --print-architecture)
+if [[ $x == "amd64" ]]; then
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.tgz
 tar -xzvf ngrok-stable-linux-arm64.tgz
-else if $x == "arm" then
-https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+fi
+
+if [[ $x == "arm" ]]; then
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
 fi
 
 apt-get install nodejs -y
-npm install shelljs colors chalk cowsay readline request ora beeper log-symbols fs
-node spoof.js
+npm install shelljs colors chalk cowsay readline request ora beeper log-symbols$
+node spoof
+
