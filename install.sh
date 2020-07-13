3c1c2120b4f0be248de36332e495078dcbd3fc57
FajarTheGGman <35714197+FajarTheGGman@users.noreply.github.com>
2020-07-13 07:37:54 +0700

    update fix bug

--- install.sh
+++ install.sh
@@ -3,26 +3,40 @@ rm -rf node_modules
 echo "[!] Installing package"
 sleep 1
 
-cek=(ls | grep "ngrok")
-amd=$(dpkg --print-architecture | grep "amd64")
-arm=$(dpkg --print-architecture | grep "arm")
+cek=$(ls | grep "ngrok")
+node=$(apt list --installed | grep "nodejs")
+module=$(ls | grep "node_modules")
 
-if [[ $cek == "ngrok" ]]; then
+install_ngrok(){
+  amd=$(dpkg --print-architecture | grep "amd64")
+  arm=$(dpkg --print-architecture | grep "arm")
+  if [[ $amd == "amd64" ]]; then
+    wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+    unzip ngrok-stable-linux-amd64.zip
+  fi
 
+  if [[ $arm == "arm" ]]; then
+    wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+    unzip ngrok-stable-linux-arm.zip
+  fi
+}
+
+if [[ $cek == *"ngrok"* ]]; then
+  echo "[+] Ngrok already downloaded";
 else
-
-if [[ $amd == "amd64" ]]; then
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
+  install_ngrok
 fi
 
-if [[ $arm == "arm" ]]; then
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
-unzip ngrok-stable-linux-arm.zip
+if [[ $node == *"nodejs"* ]]; then
+    echo "[+] Nodejs Already Installed"
+else
+    apt-get install nodejs -y
 fi
 
+if [[ $module == *"node_modules"* ]]; then
+    echo "[+] node_modules already setup"
+else
+    npm install
 fi
 
-apt-get install nodejs -y
-npm install
 node spoof
