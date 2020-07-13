02e0bd52b3fac1cd2ba62d8b80a57026bdfa2f50
FajarTheGGman <35714197+FajarTheGGman@users.noreply.github.com>
2020-07-13 07:39:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,19 @@ module=$(ls | grep "node_modules")
 install_ngrok(){
   amd=$(dpkg --print-architecture | grep "amd64")
   arm=$(dpkg --print-architecture | grep "arm")
+  aarch=$(dpkg --print-architecture | grep "aarch64")
+
   if [[ $amd == "amd64" ]]; then
     wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
     unzip ngrok-stable-linux-amd64.zip
   fi
 
   if [[ $arm == "arm" ]]; then
+    wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+    unzip ngrok-stable-linux-arm.zip
+  fi
+
+  if [[ $aarch == "aarch64" ]]; then
     wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
     unzip ngrok-stable-linux-arm.zip
   fi
